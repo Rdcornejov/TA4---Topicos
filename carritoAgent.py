24988caf755fdd60108ac3ec6da1ebc2580e58f4
Ronaldo Cornejo <66271146+Rdcornejov@users.noreply.github.com>
2023-11-16 09:06:21 -0500

    Update carritoAgent.py

--- carritoAgent.py
+++ carritoAgent.py
@@ -8,26 +8,22 @@ class CarritoAgent(Agent):
     def __init__(self) -> None:
         self.x = random.randint(30, 100)
         self.y = random.randint(30, 100)
-        self.size = 15
+        self.size = random.randint(10, 20)
         self.speed = 10 * 25 / self.size
         self.status = -1
 
     def updateStatus(self):
-        if self.y < 550:
-            self.status = 4
-            if self.x > 40:
+        if self.y < 600 and self.y >= 500:
+            if self.x >= 30 and self.x <= 100:
                 self.status = 1
-
-        elif self.y > 550:
-            self.status = 3;
-            if self.x < 40 :
-                self.status = 1
-        else:
-            if self.x < 40:
-                self.status = 1
-            else:
+            if self.x < 600 and self.x >= 500:
+                self.status = 3
+        if self.y < 100 and self.y >= 30:
+            if self.x >= 500 and self.x <= 600:
                 self.status = 2
-
+            if self.x < 100 and self.x >= 30:
+                self.status = 4
+                
     def swim(self):
         if self.status == 1: self.x += self.speed #Que se vaya a la derecha
         elif self.status == 2: self.x -= self.speed #Que se vaya  la izquierda
