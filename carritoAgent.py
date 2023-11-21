db0c34ae6aebb176230269ec337d9de02c01da8c
JeffersonEspinalA <89090023+JeffersonEspinalA@users.noreply.github.com>
2023-11-21 07:55:03 -0500

    Add files via upload

--- carritoAgent.py
+++ carritoAgent.py
@@ -5,29 +5,54 @@ from pade.core.agent import Agent
 from PySide6.QtGui import QColor
 
 class CarritoAgent(Agent):
-    def __init__(self) -> None:
-        self.x = random.randint(30, 100)
-        self.y = random.randint(30, 100)
+    def __init__(self, aid):
+        super(CarritoAgent, self).__init__(aid=aid, debug=False)
         self.size = random.randint(10, 20)
-        self.speed = 10 * 25 / self.size
-        self.status = -1
+        self.x1_limit = random.randint(30, 125 - self.size * 2)
+        self.x2_limit = random.randint(514, 609 - self.size * 2)
+        self.y1_limit = random.randint(30, 125 - self.size)
+        self.y2_limit = random.randint(514, 609 - self.size)
+        self.x = random.randint(30, 125 - self.size * 2)
+        self.y = random.randint(30, 125 - self.size * 2)
+        self.speed = 10 * 15 / self.size
+        self.status = 2
+        self.start_colision = False
+        self.vivo = True
 
     def updateStatus(self):
-        if self.y < 600 and self.y >= 500:
-            if self.x >= 30 and self.x <= 100:
-                self.status = 1
-            if self.x < 600 and self.x >= 500:
+        if self.y <= self.y1_limit:
+            self.status = 1
+            if self.x >= self.x2_limit:
+                self.status = 4
+
+        elif self.y >= self.y2_limit:
+            self.status = 2
+            if self.x <= self.x1_limit:
                 self.status = 3
-        if self.y < 100 and self.y >= 30:
-            if self.x >= 500 and self.x <= 600:
-                self.status = 2
-            if self.x < 100 and self.x >= 30:
+
+        else:
+            if self.x <= self.x1_limit:
+                self.status = 3
+            elif self.x >= self.x2_limit:
                 self.status = 4
                 
     def swim(self):
-        if self.status == 1: self.x += self.speed #Que se vaya a la derecha
+        if self.status == 1: self.x += self.speed   #Que se vaya a la derecha
         elif self.status == 2: self.x -= self.speed #Que se vaya  la izquierda
-        elif self.status == 3: self.y -= self.speed #Que se vaya para arriba
-        elif self.status == 4: self.y += self.speed  #Que se vaya para abajo
+        elif self.status == 3: 
+            self.y -= self.speed #Que se vaya para arriba
+            self.start_colision = True #Activar funcionalidad de colision
+        elif self.status == 4: self.y += self.speed #Que se vaya para abajo
 
+    def detect_collision(self, others):
+        for other in others:
+            if other != self and self.is_close_to(other):
+                self.avoid_collision()
 
+    def is_close_to(self, other):
+        distance_threshold = self.size + other.size
+        distance = ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+        return distance < distance_threshold
+
+    def avoid_collision(self):
+        self.vivo = False
--- hostagent.py
+++ hostagent.py
@@ -1,5 +1,6 @@
 import sys
 import threading
+import time
 from PySide6.QtWidgets import QApplication
 from pade.acl.aid import AID
 from pade.behaviours.protocols import TimedBehaviour
@@ -10,6 +11,16 @@ from pade.core.agent import Agent
 from globals import Global
 from gui import Gui
 
+class TimeColision(TimedBehaviour):
+    def __init__(self, agent, time):
+        super(TimeColision, self).__init__(agent, time)
+        self.agent = agent
+    
+    def on_time(self):
+        super(MyTimedBehaviour, self).on_time()
+        for fish in self.agent.fish_list:
+            fish.detect_collision(self.agent.fish_list)
+
 class MyTimedBehaviour(TimedBehaviour):
     def __init__(self, agent, time):
         super(MyTimedBehaviour, self).__init__(agent, time)
@@ -17,43 +28,39 @@ class MyTimedBehaviour(TimedBehaviour):
 
     def on_time(self):
         super(MyTimedBehaviour, self).on_time()
-        # display_message(self.agent.aid.localname, 'Updating the fishies!')
-        for fish in self.agent.fish_list:
-            fish.updateStatus()
-            fish.swim()
-
         gui.update()
 
-class YourTimedBehaviour(TimedBehaviour):
+class movimientoCarro(TimedBehaviour):
     def __init__(self, agent, time):
-        super(YourTimedBehaviour, self).__init__(agent, time)
+        super(movimientoCarro, self).__init__(agent, time)
         self.agent = agent
 
     def on_time(self):
-        super(YourTimedBehaviour, self).on_time()
-        Global.x_center += 3
-        print(Global.x_center)
+        super(movimientoCarro, self).on_time()
+        self.agent.updateStatus()
+        self.agent.swim()
 
 class HostAgent(Agent):
     gui = None
-    num_fishes = 2
+    num_fishes = 20
     fish_list = []
     enabled = False
-
-    def __init__(self, aid):
-        super(HostAgent, 
-              self).__init__(aid=aid, debug=False)
-
+    agents = []
+    port = int(sys.argv[1])
+    def __init__(self, aid, agents):
+        super(HostAgent, self).__init__(aid=aid, debug=False)
+        self.agents = agents
         Global.x_center = 0
 
-        for _ in range(self.num_fishes):
-            self.fish_list.append(CarritoAgent())
-            # TODO name it and launch it as an agent of chaos
+        for i in range(self.num_fishes):
+            pes_agent_name = 'pes_agent_{}@localhost:{}'.format(port+i, port+i)
+            fish = CarritoAgent(AID(name=pes_agent_name))
+            self.fish_list.append(fish)
+            self.behaviours.append(movimientoCarro(fish, .2))
+            self.agents.append(fish)
 
         mytimed = MyTimedBehaviour(self, .2)
-        yourtimed = YourTimedBehaviour(self, 2)
-        self.behaviours.append(mytimed)
-        self.behaviours.append(yourtimed)
+        self.behaviours.append(mytimed)    
 
 def agentsexec():
     start_loop(agents)
@@ -61,8 +68,8 @@ def agentsexec():
 if __name__ == '__main__':
     agents = list()
     port = int(sys.argv[1])
-    host_agent_name = 'host_agent_{}@localhost:{}'.format(port+1, port+1)
-    host_agent = HostAgent(AID(name=host_agent_name))
+    host_agent_name = 'host_agent_{}@localhost:{}'.format(port+1000, port+1000)
+    host_agent = HostAgent(AID(name=host_agent_name), agents)
     agents.append(host_agent)
 
     x = threading.Thread(target=agentsexec)
